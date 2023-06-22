b20b2de7f7996bf0f9a17e697346d67203f70d1b
LJH <ljf135798@naver.com>
2023-06-22 15:54:33 +0900

    1111

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ $(function () {
     const MainSlide = new Swiper('.main_slide', {
         loop: true,
         parallax: true,
-        speed: 600,
+        speed: 1600,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false,
@@ -49,13 +49,20 @@ $(function () {
 
     const ItmSlide = new Swiper('.itm_slide', {
         loop: true,
-        slidesPerView: 2,
-        spaceBetween: 400,
+        slidesPerView: 1,
+        spaceBetween: 0,
         speed: 900,
         autoplay: {
             delay: 4000,
             disableOnInteraction: false,
         },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 400,
+            }
+        }
     });
 
     $('.MainItm .arrows .left').on('click', function () {
@@ -68,6 +75,21 @@ $(function () {
     // document.querySelector('.MainItm .arrows .left').addEventListener('click', () => {
     //     ItmSlide.slidePrev();
     // });
+
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.Gnb').toggleClass('on');
+    });
+
+    $('.Gnb>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).parent().siblings().find('.snb').stop().slideUp();
+    });
+
+    $(window).on('resize', function () {
+        $('.Gnb .snb').removeAttr('style')
+    })
 
 
 
